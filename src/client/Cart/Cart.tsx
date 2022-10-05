1051d4b990b6bec3c148197a39f0b7d32dfa6f1b
Mary <marylychee@pm.me>
2022-10-05 16:21:41 +1000

    purchase button

--- src/client/Cart/Cart.tsx
+++ src/client/Cart/Cart.tsx
@@ -1,4 +1,5 @@
 import CartItem from './CartItem/CartItem';
+import Button from '@material-ui/core/Button';
 import { Wrapper } from './Cart.styles';
 import { CartItemType } from '../App';
 
@@ -6,9 +7,10 @@ type Props = {
   cartItems: CartItemType[];
   addToCart: (clickedItem: CartItemType) => void;
   removeFromCart: (id: number) => void;
+  handlePurchase: () => void;
 };
 
-const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart, handlePurchase }) => {
   const calculateTotal = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
 
@@ -25,6 +27,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
         />
       ))}
       <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+      {cartItems.length === 0 ? null : <Button onClick={() => handlePurchase()} variant="contained">Purchase</Button>}
     </Wrapper>
   );
 };
