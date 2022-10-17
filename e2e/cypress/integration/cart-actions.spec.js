27599d5ec122bd55f4fd3a751818786cd7a3b125
Mary <marylychee@pm.me>
2022-10-17 14:22:30 +1000

    add cypress UI testing

--- e2e/cypress/integration/cart-actions.spec.js
+++ e2e/cypress/integration/cart-actions.spec.js
@@ -14,4 +14,15 @@ context('Cart Actions', () => {
 
   })
 
+  it('Performs the Purchase action ', () => {
+
+    cy.get('[data-cy=add-to-cart-2]').click();
+    cy.get('[data-cy=add-to-cart-3]').click();
+    cy.get('[data-cy=open-cart').click();
+    cy.get('[data-cy=purchase').click();
+
+    cy.get('[data-cy=cart]').should('include.text', 'Purchase Successful');
+
+  })
+
 })
